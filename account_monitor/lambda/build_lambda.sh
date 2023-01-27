26980b0187a31a90d379e5e2f4c7c6efecdd8400
Chris Rose <chris.rose@snyk.io>
2023-01-27 16:31:20 +0000

    CloudFormation Template to deploy Account Monitor Lambda

---
+++ account_monitor/lambda/build_lambda.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install package dependencies
+ pip3 install -r requirements.txt --target ./package
+
+# Zip up the package dependencies 
+ zip -r aws-account-monitor.zip ./package
+
+# Add the main lambda code to the Zip file
+ zip aws-account-monitor.zip main.py
